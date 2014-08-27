3056d00db41da98580e056824d3f6bf13d9183b3
tanitta <ttata.trit@gmail.com>
2014-08-27 17:15:55 +0900

    first commit

---
+++ Core.lua
@@ -0,0 +1,15 @@
+namespace "trit"{
+	namespace "typ"{
+		class "Core"{
+			metamethod"_init"
+			:body(function(self)
+			end);
+
+			metamethod"__call"
+			:body(function(self)
+			end);
+
+
+		}
+	}--typ
+}--trit
